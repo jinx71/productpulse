3c74cf0ab06702e9975725eec72518ccc4ec407b
jinx71 <karim.sazed@gmail.com>
2022-02-18 03:19:07 +0000

    feat: define User model

---
+++ productpulse/server/src/models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+      minlength: [2, 'Name must be at least 2 characters'],
+      maxlength: [40, 'Name must be at most 40 characters'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [6, 'Password must be at least 6 characters'],
+      select: false, // never returned by default queries
+    },
+    avatarColor: {
+      type: String,
+      default: '#FF6B5C', // coral — used for generated initials avatars on the client
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash the password whenever it is set/changed.
+userSchema.pre('save', async function hashPassword(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Instance helper used by the login controller.
+userSchema.methods.matchPassword = function matchPassword(candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
+
+// Belt-and-braces: strip password if a document is ever serialized to JSON.
+userSchema.methods.toJSON = function toJSON() {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+module.exports = mongoose.model('User', userSchema);
